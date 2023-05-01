2f861e49e99a314eb79e1ac0766b09be24e343c1
Andrew Dibble <andrewdibble14@outlook.com>
2023-05-01 11:20:42 -0700

    worked on file and password input from user

--- java_solution/src/main.java
+++ java_solution/src/main.java
@@ -1,16 +1,26 @@
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
+/**
+ * things to do:
+ * error log file
+ * output file
+ * password hashing to validate
+ */
 public class main {
+    static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args){
         nameInput();
         intInput();
         fileInput();
+        passwordInput();
     }
 
     public static void nameInput(){
-        Scanner input = new Scanner(System.in);
         System.out.println("First and Last Name must consist of only alphabetical symbols. Upper and lower case is fine. Maximum 50 symbols.");
 
         System.out.println("Enter First Name: ");
@@ -37,7 +47,6 @@ public class main {
     }
 
     public static void intInput(){
-        Scanner input = new Scanner(System.in);
         System.out.println("Integers can be positive or negative within the range of -2147 and 2147.");
 
         System.out.println("Enter an Integer: ");
@@ -70,11 +79,11 @@ public class main {
      *checks if an integer is in the correct bounds
      */
     public static boolean checkBounds(String theInt){
-        int num = Integer.parseInt(theInt);
-        if(num > 2147){
+        long num = Long.parseLong(theInt);
+        if(num > 2147483647){
             System.out.println("Given integer exceeds maximum allowed value.");
             return false;
-        }else if(num < -2147){
+        }else if(num < -2147483648){
             System.out.println("Given integer exceeds minimum allowed value.");
             return false;
         }
@@ -83,6 +92,56 @@ public class main {
     }
 
     public static void fileInput(){
+        System.out.println("File Names not to exceed 20 symbols. Only alphabet, numbers, underscore accepted. Case insensitive. Only .txt files accepted");
+
+        System.out.println("Enter an Input File Name: ");
+        boolean correctInputFileName = false;
+        while(!correctInputFileName) {
+            String inputName = input.next();
+            correctInputFileName = patternMatcherHelper(inputName, "^\\w{1,20}\\.txt$");
+            if (correctInputFileName)
+                System.out.println("Input file name: " + correctInputFileName);
+            else //there was an error in the input
+                System.out.println("Enter an Input File Name: ");
+        }
+
+        System.out.println("Enter an Output File Name: ");
+        boolean correctOutputFileName = false;
+        while(!correctOutputFileName) {
+            String outputName = input.next();
+            correctOutputFileName = patternMatcherHelper(outputName, "^\\w{1,20}\\.txt$");
+            if (correctOutputFileName)
+                System.out.println("Output file name: " + correctOutputFileName);
+            else //there was an error in the input
+                System.out.println("Enter an Output File Name: ");
+        }
+    }
+
+    public static void passwordInput(){
+        System.out.println("Password should contain at least one uppercase letter, at least one lowercase letter, at least one digit, at least one symbol, and no more than three consecutive lowercase letters.");
+
+        System.out.println("Enter Password: ");
+        boolean correctPassword = false;
+        String password = "";
+        while(!correctPassword) {
+            password = input.next();
+            correctPassword = patternMatcherHelper(password, "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])(?![a-z]{4,}).{10,}$");
+            if (correctPassword)
+                System.out.println("The Password: " + correctPassword);
+            else //there was an error in the input
+                System.out.println("Enter Password: ");
+        }
+
+        System.out.println("Please re-enter Password");
+        Scanner newInput = new Scanner(System.in);
+        String verifyPassword = newInput.next();
+
+        while(!Objects.equals(password, verifyPassword)){
+            System.out.println("Passwords do not match");
+            System.out.println("Please re-enter Password");
+        }
+
+        System.out.println("The Passwords match");
 
     }
 
