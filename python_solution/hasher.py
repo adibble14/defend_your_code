0bd8d56bf4ccca595de408610d8a655e5ed2f619
mfvcreative <mfvcreative@gmail.com>
2023-05-03 10:06:33 -0700

    Working on hasher functions.

--- python_solution/hasher.py
+++ python_solution/hasher.py
@@ -1,4 +1,20 @@
 # adds salt and hashes a password
+# technique from https://docs.python.org/3/library/secrets.html
+import secrets
+import string
+import hashlib
 
+def get_salt():
+    alphabet = string.ascii_letters + string.digits
+    while True:
+        salt = ''.join(secrets.choice(alphabet) for i in range(10))
+        if(any(c.isupper() for c in salt)
+            and any(c.isupper() for c in salt)
+            and sum(c.isdigit() for c in salt) >= 3):
+            break
+        return salt
+    
+def hash(str):
+    return hashlib.sha256(b"str").hexdigest()
 
 
